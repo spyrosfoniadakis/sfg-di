46af1cecd30a2241a00c78b9717dbfb51818cb13
Spyros Foniadakis <spyrosfnd@gmail.com>
2020-02-27 11:26:17 +0200

    Spring DI using primary qualifier

---
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService{
+    @Override
+    public String greet() {
+        return "Primary Greeting Service greeting: Hello world!";
+    }
+}
